1696b13e1523f701d666393e9d40129662e692cf
Anton <antob.chebanitsa@gmail.com>
2020-11-20 22:59:42 +0300

    Almost done.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Homework.IT_Academy2
 {
@@ -6,9 +10,43 @@ namespace Homework.IT_Academy2
     {
         static void Main(string[] args)
         {
-            string path = "D:\\Homework.IT_Academy2\\Cars.json";
-            var station = new TaxiStation(path);
-            station.SummaryCost()
+            string pathEl = "D:\\Homework.IT_Academy2\\ElectroCars.json";
+            string pathFu = "D:\\Homework.IT_Academy2\\FuelCars.json";
+
+            string content1 = File.ReadAllText(pathEl);
+            string content2 = File.ReadAllText(pathFu);
+
+            ElectroCar[] elCars = JsonConvert.DeserializeObject<ElectroCar[]>(content1);
+            FuelCar[] fuCars = JsonConvert.DeserializeObject<FuelCar[]>(content2);
+
+            Car[] cars = new Car[elCars.Length + fuCars.Length];
+
+            for (int i = 0; i < elCars.Length; i++)
+            {
+                cars[i] = elCars[i];
+            }
+
+            for (int i = 0; i < fuCars.Length; i++)
+            {
+                cars[elCars.Length + i] = fuCars[i];
+            }
+
+            var ts = new TaxiStation(cars);
+            var a = ts.SummaryCost();
+
+            Console.WriteLine("Cars before sorting");
+
+            foreach (var car in ts.Cars)
+            {
+                Console.WriteLine(car.Name + "=" + car.Cost);
+            }
+            ts.SortByCost();
+            Console.WriteLine("Cars after sorting");
+            foreach (var car in ts.Cars)
+            {
+                Console.WriteLine(car.Name + "=" + car.Cost);
+            }
+
         }
     }
 }
--- TaxiStation.cs
+++ TaxiStation.cs
@@ -9,32 +9,44 @@ namespace Homework.IT_Academy2
 {
     class TaxiStation
     {
-        public Car[] Cars { get; }
-        double sum = 0;
-        Car SortByConsumption(double consumption)
+        public Car[] Cars { get; set; }
+
+        public TaxiStation(Car[] arrCars)
         {
-            return null;
+            Cars = arrCars;
         }
-        public void FindBySpeed(Car[] car)
+
+        public void SortByCost()
+        {
+            Car value;
+            for (int i = 0; i < Cars.Length - 1; i++)
+            {
+                for (int j = i + 1; j < Cars.Length; j++)
+                {
+                    if (Cars[i].Cost > Cars[j].Cost)
+                    {
+                        value = Cars[i];
+                        Cars[i] = Cars[j];
+                        Cars[j] = value;
+                    }
+                }
+            }
+        }
+        public void FindBySpeed(Car[] c)
         {
             Console.WriteLine("Enter speed: ");
-            int enteredSpeed = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < Cars.Length; i++)
-                if (enteredSpeed = )
-                    Console.WriteLine("");
-            Console.WriteLine();
+            var enteredSpeed = Convert.ToInt32(Console.ReadLine());
+            foreach (var car in Cars)
+                Console.WriteLine(enteredSpeed == car.Speed ? $"Founded car {car.Name}" : "Not found");
         }
-        public double SummaryCost(int cost)
+        public int SummaryCost()
         {
-            for (int i = 0; i < Cars.Length; i++)
-                sum += cost;
+            int sum = 0;
+            foreach (var car in Cars)
+                sum += car.Cost;
             return sum;
         }
-        public TaxiStation(string filePath)
-        {
-            string content = File.ReadAllText(filePath);
-            Cars = JsonConvert.DeserializeObject<Car[]>(content);
-        }
+
     }
 }
 

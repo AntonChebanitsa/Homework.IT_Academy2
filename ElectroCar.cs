90d07c04ddebabed48d7cd9ff4bfbdf4ad068d7e
Anton <antob.chebanitsa@gmail.com>
2020-11-22 21:00:28 +0300

    Added nested clases. Need fix

--- ElectroCar.cs
+++ ElectroCar.cs
@@ -4,31 +4,31 @@ namespace Homework.IT_Academy2
 {
     public class ElectroCar : Car
     {
-        public Battery Battery { get; }
+        public Battery BatteryProp { get; }
 
         public ElectroCar(string name, int speed, int cost, string typeFuel, Battery battery)
             : base(name, speed, cost, typeFuel)
         {
-            Battery = battery;
+            BatteryProp = battery;
         }
 
-        public override void  Move()
+        public override void Move()
         {
             Console.WriteLine("I'm moving absolutely silently");
         }
-    }
 
-    public class Battery
-    {
-        public string Manufacturer { get; set; }
-        public int ElectroCapacity { get; set; }
-        public int ElectroConsumption { get; set; }
-
-        public Battery(string manufacturer, int capacity, int consumption)
+        public class Battery
         {
-            Manufacturer = manufacturer;
-            ElectroCapacity = capacity;
-            ElectroConsumption = consumption;
+            public string Manufacturer { get; set; }
+            public int ElectroCapacity { get; set; }
+            public int ElectroConsumption { get; set; }
+
+            public Battery(string manufacturer, int capacity, int consumption)
+            {
+                Manufacturer = manufacturer;
+                ElectroCapacity = capacity;
+                ElectroConsumption = consumption;
+            }
         }
     }
 }
